037a7fb786031effe72407900a9cce7981ca94c6
rajkumar-101 <rajmummies@gmail.com>
2020-08-23 12:27:19 +0530

    Updates

---
+++ precache-manifest.b50b5b3e08a701be5ae24454c954a9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e228ba9f01e466f62c849fda4a771b",
+    "url": "/firegram-practice/index.html"
+  },
+  {
+    "revision": "a2037fe66701ad66c16a",
+    "url": "/firegram-practice/static/css/main.dac3dfc1.chunk.css"
+  },
+  {
+    "revision": "504815fd25e30a57d9d7",
+    "url": "/firegram-practice/static/js/2.b32ec1a9.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram-practice/static/js/2.b32ec1a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2037fe66701ad66c16a",
+    "url": "/firegram-practice/static/js/main.c29ee371.chunk.js"
+  },
+  {
+    "revision": "06738438b8c91ceced34",
+    "url": "/firegram-practice/static/js/runtime-main.3e46bb81.js"
+  }
+]);
